034d46295f7810b277a8c960796bb98498938124
MaryWindows <anne.maggot@gmail.com>
2021-11-13 01:16:56 +0200

    Setting OnClickListeners on buttons

--- app/src/main/java/com/example/lesson_four_exercise_one/MainActivity.kt
+++ app/src/main/java/com/example/lesson_four_exercise_one/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.lesson_four_exercise_one
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvNewGame.setOnClickListener{
+        }
 
+        tvAbout.setOnClickListener{
+
+        }
+
+        tvContinue.setOnClickListener{
+
+        }
+
+        tvOptions.setOnClickListener{
+
+        }
 
     }
 }
